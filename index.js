fccaae8a495c23761c2499777111936e623d83a1
Iara Flaminio <136760650+iaraflaminio@users.noreply.github.com>
2024-10-02 22:56:39 -0300

    add the playGame function with 5 rounds

--- index.js
+++ index.js
@@ -38,3 +38,12 @@ function playRound(humanChoice, computerChoice) {
     }
 
 }
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        console.log(`Round ${i + 1}:`)
+        const humanSelection = getHumanChoice()
+        const computerSelection = getComputerChoice()
+        playRound(humanSelection, computerSelection)
+    }
+}
